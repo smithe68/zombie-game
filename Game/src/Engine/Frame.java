3c1f92bf4271eccc633c6bd443c1dff22cef6586
smithe68 <Evanelectric@gmail.com>
2018-04-26 22:46:31 -0700

    made some from stuff

--- Game/src/Engine/Frame.java
+++ Game/src/Engine/Frame.java
@@ -1,20 +1,25 @@
 package Engine;
 import com.sun.javaws.util.JfxHelper;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Frame{
 
     JFrame frame = new JFrame();
 
     public Frame(){
+
         frame.setVisible(true);
+
     }
 
     private void window(){
+
+
         frame.add(frame);
         frame.setSize(1920,1080);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+
     }
 }

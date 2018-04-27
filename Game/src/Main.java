73d2727e3d1c1b72b0fb3c42540e7b4d779d8864
Jakub <jakubshark@gmail.com>
2018-04-26 22:13:07 -0700

    Hello World

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-
+/**
+ * This is the starting point fo the program
+ * @author Evan Smith
+ * */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
     }
 }
